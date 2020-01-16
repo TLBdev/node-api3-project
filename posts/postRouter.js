e257371cdaadde36bde479b04b0fb62e8964d013
tony <serratedhumor@yahoo.com>
2020-01-15 18:59:56 -0600

    mvp and stretch done... oops

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,61 @@
 const express = require('express');
-
+const database = require('./postDb')
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
+  database.get()
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Server side error'
+      })
+    })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  database.getById(req.params.id)
+    .then(data => {
+      res.status(201).json(data)
+    })
+    .catch(err => {
+      res.status(500).json({ message: '500 error' })
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  database.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: 'delete successful' })
+    })
+    .catch(err => {
+      res.status(500).json({ message: '500 error' })
+    })
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
+  database.update(req.params.id, req.body)
+    .then(() => {
+      res.status(200).json({ message: 'update successful' })
+    })
+    .catch(err => {
+      res.status(500).json({ message: '500 error' })
+    })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  if (!req.body.text) {
+    res.status(400).json({ message: '400: Bad Request' })
+  } else {
+    next()
+  }
 }
 
 module.exports = router;
